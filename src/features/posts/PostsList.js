c7138ffa3ece7f5f2c93654a26b2c873ecdd2eb7
Anicet Togue <atogue@gmail.com>
2020-11-24 13:18:04 +0100

    add date reference into every post and sort posts in reverse order

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { postDeleted } from './postsSlice'
 import { Link}  from 'react-router-dom'
 import {PostAuthor} from "./PostAuthor";
+import {TimeAgo} from "./TimeAgo";
 
 export const PostsList = () => {
     const posts = useSelector(state => state.posts)
@@ -19,10 +20,13 @@ export const PostsList = () => {
             )
         }
     }
+    //Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = posts.map(post => (
+    const renderedPosts = orderedPosts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
+            <TimeAgo timestamp={post.date} />
             <p className="post-content">{post.content.substring(0, 100)}</p>
             <button className="post-deleted"
                 type="button"
