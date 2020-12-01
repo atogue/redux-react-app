6c3f65352d134385ba2fa39e1746d2679bf23ac6
Anicet Togue <atogue@gmail.com>
2020-12-01 14:08:18 +0100

    create component to handle view post and replace postsList with fetch fakeApi posts

---
+++ src/features/posts/PostExcerpt.js
@@ -0,0 +1,37 @@
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
+import { Link } from 'react-router-dom'
+import { postDeleted } from './postsSlice'
+import { useDispatch } from 'react-redux'
+
+export const PostExcerpt = ({post}) => {
+    const dispatch = useDispatch()
+
+    const onDeletePostClicked = (e, id) => {
+        e.preventDefault()
+        if (id) {
+            dispatch(
+                postDeleted({
+                    id: id
+                })
+            )
+        }
+    }
+
+    return (
+        <article className="post-excerpt" key={post.id}>
+            <h3>{post.title}</h3>
+            <PostAuthor userId={post.user} />
+            <TimeAgo timestamp={post.date} />
+            <p className="post-content">{post.content.substring(0, 100)}</p>
+            <ReactionButtons post={post} />
+            <button className="post-deleted"
+                    type="button"
+                    onClick={(e) => onDeletePostClicked(e, post.id)}>delete post</button>
+
+            <Link to={`/posts/${post.id}`} className="button muted-button">View Post</Link>
+
+        </article>
+    )
+}
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,50 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
-import { postDeleted } from './postsSlice'
-import { Link }  from 'react-router-dom'
-import { PostAuthor } from './PostAuthor'
-import { TimeAgo } from './TimeAgo'
-import { ReactionButtons } from './ReactionButtons'
-import { selectAllPosts } from './postsSlice'
+import { selectAllPosts, fetchPosts } from './postsSlice'
+import {PostExcerpt} from './PostExcerpt'
 
 export const PostsList = () => {
     const posts = useSelector(selectAllPosts)
-
+    const postStatus = useSelector(state => state.posts.status)
     const dispatch = useDispatch()
-    const onDeletePostClicked = (e, id) => {
-        e.preventDefault()
-        if (id) {
-            dispatch(
-                postDeleted({
-                    id: id
-                })
-            )
+    const error = useSelector(state => state.posts.error)
+
+    useEffect(() => {
+        if (postStatus === 'idle') {
+            dispatch(fetchPosts())
         }
+    }, [postStatus, dispatch])
+
+    let content
+    if (postStatus === 'loading') {
+        content = <div className="loader">Loading...</div>
+    } else if (postStatus === 'succeeded') {
+        // Sort posts in reverse chronological order by datetime string
+        const orderedPosts = posts
+            .slice()
+            .sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => (
+            <PostExcerpt key={post.id} post={post} />
+        ))
+    } else if (postStatus === 'failed') {
+        content = <div>{error}</div>
     }
-    //Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date))
-
-    const renderedPosts = orderedPosts.map(post => (
-        <article className="post-excerpt" key={post.id}>
-            <h3>{post.title}</h3>
-            <PostAuthor userId={post.user} />
-            <TimeAgo timestamp={post.date} />
-            <p className="post-content">{post.content.substring(0, 100)}</p>
-            <ReactionButtons post={post} />
-            <button className="post-deleted"
-                type="button"
-                onClick={(e) => onDeletePostClicked(e, post.id)}>delete post</button>
-
-            <Link to={`/posts/${post.id}`} className="button muted-button">View Post</Link>
-
-        </article>
-    ))
 
     return (
         <section className="posts-list">
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
