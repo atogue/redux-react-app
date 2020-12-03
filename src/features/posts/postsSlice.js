57cecd7a6b76b6e83e4e2f1368422297fa542121
Anicet Togue <atogue@gmail.com>
2020-12-03 21:03:58 +0100

    refactoring: use createEntityAdapter api to handle fetch posts - add new post and delete post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,11 +1,17 @@
-import { createSlice, createAsyncThunk, createSelector } from '@reduxjs/toolkit'
+import {
+    createSlice,
+    createAsyncThunk,
+    createSelector,
+    createEntityAdapter } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
 
-const initialState = {
-    posts: [],
+const postsAdapter = createEntityAdapter({
+    sortComparer: (a, b) => b.date.localeCompare(a.date)
+})
+const initialState = postsAdapter.getInitialState({
     status: 'idle',
     error: null
-}
+})
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     const response = await client.get('/fakeApi/posts')
@@ -27,28 +33,17 @@ const postsSlice = createSlice({
     reducers: {
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload
-            const existingPost = state.posts.find(post => post.id === postId)
+            const existingPost = state.entities[postId]
             if (existingPost) {
                 existingPost.reactions[reaction] = existingPost.reactions[reaction] +1
             }
         },
         postDeleted(state, action) {
-            let index = null
-            // eslint-disable-next-line
-            state.posts.filter(post => {
-                if (post.id === action.payload.id) {
-                    index = state.posts.indexOf(post)
-                }
-            })
-
-            if (index > -1) {
-               // console.log('post found with index (' + index + ') and deleted from state!')
-                state.posts.splice(index, 1)
-            }
+            postsAdapter.removeOne(state, action.payload.id)
         },
         postUpdated(state, action) {
             const { id, title, content } = action.payload
-            const existingPost = state.posts.find(post => post.id === id)
+            const existingPost = state.entities[id]
             if (existingPost) {
                 existingPost.title = title
                 existingPost.content = content
@@ -61,8 +56,9 @@ const postsSlice = createSlice({
         },
         [fetchPosts.fulfilled]: (state, action) => {
             state.status = 'succeeded'
-            //add any fetched posts to the array
-            state.posts = state.posts.concat(action.payload)
+            // add any fetched posts to the array
+            // Use the `upsertMany` reducer as a mutating update utility
+            postsAdapter.upsertMany(state, action.payload)
         },
         [fetchPosts.rejected]: (state, action) => {
             state.status = 'failed'
@@ -74,7 +70,8 @@ const postsSlice = createSlice({
         [addNewPost.fulfilled]: (state, action) => {
             state.status = 'succeeded'
             // add directly the new post object to our posts array
-            state.posts.push(action.payload)
+            // Use the `addOne` reducer for the fulfilled case
+            postsAdapter.addOne(state, action.payload)
         },
         [addNewPost.rejected]: (state, action) => {
             state.status = 'failed'
@@ -87,9 +84,12 @@ export const { postAdded, postDeleted, postUpdated, reactionAdded } = postsSlice
 
 export default postsSlice.reducer
 
-export const selectAllPosts = state => state.posts.posts
-
-export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
+// export the customized selectors for this adapter using `getSelectors`
+export const {
+    selectAll: selectAllPosts,
+    selectById: selectPostById,
+    selectIds: selectPostIds
+} = postsAdapter.getSelectors(state => state.posts)
 
 // memoized selector function to retrieve all posts by userId
 export const selectPostsByUser = createSelector(
