a234e2fc68cdef0af9d3265e429b338ba8ba267a
Anicet Togue <atogue@gmail.com>
2020-12-03 14:12:18 +0100

    fetch and display new notifications with a green style

--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { formatDistanceToNow, parseISO } from 'date-fns'
+import classnames from 'classnames'
 
 import { selectAllUsers } from '../users/usersSlice'
-import { selectAllNotifications } from './notificationsSlice'
+import { selectAllNotifications, allNotificationsRead } from './notificationsSlice'
 
 export const NotificationsList = () => {
+    const dispatch = useDispatch()
     const notifications = useSelector(selectAllNotifications)
     const users = useSelector(selectAllUsers)
 
+    useEffect(() => {
+        dispatch(allNotificationsRead())
+    })
     const renderedNotifications = notifications.map(notification => {
         const date = parseISO(notification.date)
         const timeAgo = formatDistanceToNow(date)
@@ -16,8 +21,12 @@ export const NotificationsList = () => {
             name: 'Unknow User'
         }
 
+        const notificationClassname = classnames(`${notification.isNew ? 'post-notification-new': 'post-notification'}`, {
+            new: notification.isNew //seems to be deprecated: no blink display
+        })
+
         return (
-            <div key={notification.id} className="notification">
+            <div key={notification.id} className={notificationClassname}>
                 <div>
                     <b>{user.name}</b> {notification.message}
                 </div>
