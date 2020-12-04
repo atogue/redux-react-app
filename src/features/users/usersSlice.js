79b6a1b56e59b76ab7de47bacec3d0b2ae89ee65
Anicet Togue <atogue@gmail.com>
2020-12-04 12:59:18 +0100

    refactoring - performance: converting users slice to use entity adapter api

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,12 +1,9 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
 
+const usersAdapter = createEntityAdapter()
 // warning: this initial users doesn't match to server post object - add new post with them will fail!
-const initialState = [
-    {id: '0', name: 'Anicet Togue'},
-    {id: '1', name: 'Monica Belluccia'},
-    {id: '2', name: 'Sharon Stonesa'}
-]
+const initialState = usersAdapter.getInitialState()
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
     const response = await client.get('/fakeApi/users')
@@ -19,14 +16,15 @@ const usersSlice = createSlice({
     reducers: {},
     extraReducers: {
         [fetchUsers.fulfilled]: (state, action) => {
-            // action.payload is the inMemory users
-            return action.payload ? action.payload.concat(initialState) : initialState
+            // use of adapter api : action.payload is the inMemory users
+            usersAdapter.setAll(state, action.payload)
         }
     }
 })
 
 export default usersSlice.reducer
 
-export const selectAllUsers = state => state.users
-
-export const selectUserById = (state, userId) => state.users.find(user => user.id === userId)
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById
+} = usersAdapter.getSelectors(state => state.users)
